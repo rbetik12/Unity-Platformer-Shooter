37a23fae4e9689c1d62bae1be2df8ddea2c925e9
Vitaliy Prikota <mr.prikota@gmail.com>
2020-02-02 23:06:54 +0300

    Added enemy prefab. Implemented enemy aiming.

---
+++ Platformer Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    
+    void Start() {
+
+    }
+
+    
+    void Update() {
+
+    }
+}
---
+++ Platformer Shooter/Assets/Scripts/Enemy/EnemyShooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour {
+
+    [SerializeField] private GameObject bulletObj;
+    [SerializeField] private GameObject firePoint;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float shootingPeriod;
+    private Vector3 rotation;
+
+    private void Start() {
+        StartCoroutine(Aim());
+    }
+
+    void Update() {
+    }
+
+    private void Shoot() {
+
+    }
+
+    private IEnumerator Aim() {
+        while (true) {
+            rotation = Vector3.Normalize(player.transform.position - this.transform.position);
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg);
+            yield return new WaitForSeconds(shootingPeriod);
+        }
+    }
+}
