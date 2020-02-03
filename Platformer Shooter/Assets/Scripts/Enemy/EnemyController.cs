3361c42900fdbcaf8978cd493300b13a4842d06e
Vitaliy Prikota <mr.prikota@gmail.com>
2020-02-03 17:16:42 +0300

    Added primitive pathfinding for enemy object.

--- Platformer Shooter/Assets/Scripts/Enemy/EnemyController.cs
+++ Platformer Shooter/Assets/Scripts/Enemy/EnemyController.cs
@@ -2,17 +2,57 @@
 using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
-    [SerializeField] private Transform player;
-    [SerializeField] private float speed = 200f;
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 2000f;
     [SerializeField] private float nextWaypointDistance;
 
     private Path path;
-    
-    void Start() {
+    private int currentWaypoint;
+    private bool reachedEndOfPath = false;
 
+    private Seeker seeker;
+    private Rigidbody2D rb;
+    private Vector2 force;
+
+    void Start() {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath() {
+        if (seeker.IsDone()) {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
     }
 
     void Update() {
+        if (path == null) {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count) {
+            reachedEndOfPath = true;
+            return;
+        } else {
+            reachedEndOfPath = false;
+        }
 
+        Vector2 direction = ((Vector2) path.vectorPath[currentWaypoint] - rb.position).normalized;
+        force = direction * speed * Time.deltaTime;
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance) {
+            currentWaypoint++;
+        }
+    }
+
+    private void OnPathComplete(Path path) {
+        if (!path.error) {
+            this.path = path;
+            currentWaypoint = 0;
+        }
     }
 }
