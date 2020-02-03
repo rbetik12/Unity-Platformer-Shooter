bdeba3afde4eb6ebb04c70723b93ac5896c31db2
Vitaliy Prikota <mr.prikota@gmail.com>
2020-02-03 14:50:13 +0300

    Added A star lib

--- Platformer Shooter/Assets/Scripts/Enemy/EnemyShooting.cs
+++ Platformer Shooter/Assets/Scripts/Enemy/EnemyShooting.cs
@@ -11,19 +11,24 @@ public class EnemyShooting : MonoBehaviour {
 
     private void Start() {
         StartCoroutine(Aim());
+        StartCoroutine(Shoot());
     }
 
     void Update() {
-    }
-
-    private void Shoot() {
-
     }
 
     private IEnumerator Aim() {
         while (true) {
             rotation = Vector3.Normalize(player.transform.position - this.transform.position);
             transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    private IEnumerator Shoot() {
+        while(true) {
+            GameObject bullet = Instantiate(bulletObj, firePoint.transform.position, firePoint.transform.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(rotation.x, rotation.y) * 20f;
             yield return new WaitForSeconds(shootingPeriod);
         }
     }
