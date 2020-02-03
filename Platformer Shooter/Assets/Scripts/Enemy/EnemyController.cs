5eb76611afa1efd000d9edb7491051bf5bc8cfe0
Vitaliy Prikota <mr.prikota@gmail.com>
2020-02-03 15:46:00 +0300

    Downgraded vs code extension version to fix annoying bug. Added EnemyController class.

---
+++ Platformer Shooter/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using Pathfinding;
+using UnityEngine;
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float speed = 200f;
+    [SerializeField] private float nextWaypointDistance;
+
+    private Path path;
+    
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
