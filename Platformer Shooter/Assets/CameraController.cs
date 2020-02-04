4d58eccdc5cbca486c7f264f8891a70889c47dcb
Vitaliy Prikota <mr.prikota@gmail.com>
2020-02-04 20:57:25 +0300

    Camera follows player and changes its size dependly on player's speed.

---
+++ Platformer Shooter/Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] private GameObject player;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float cameraBasicSize = 10f;
+    [SerializeField] private float cameraScaleSpeed = 2f;
+
+    private Rigidbody2D playerRb; 
+    void Start() {
+        playerRb = player.GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update() {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        mainCamera.orthographicSize = Mathf.Clamp(Mathf.MoveTowards(mainCamera.orthographicSize, cameraBasicSize + playerRb.velocity.magnitude, cameraScaleSpeed * Time.deltaTime), 10, 13);
+    }
+}
