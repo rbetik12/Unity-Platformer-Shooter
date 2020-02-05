b8a711ba81d6ea40cc6161f54bb487ed71bdc629
Vitaliy Prikota <mr.prikota@gmail.com>
2020-02-05 20:59:47 +0300

    Added TMPro package. Added death menu.

---
+++ Platformer Shooter/Assets/Scripts/UIControllers/GameUIContoller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class GameUIContoller : MonoBehaviour {
+
+    [SerializeField] private TextMeshProUGUI restartLabel;
+    [SerializeField] private LevelLoader levelLoader;
+
+    public void OnRestartEnter() {
+        restartLabel.color = new Color(66 / 256f, 135 / 256f, 245 / 256f, 1f);
+    }
+
+    public void OnRestartExit() {
+        restartLabel.color = new Color(1f, 1f, 1f, 1f);
+    }
+
+    public void OnRestartClick() {
+        restartLabel.color = new Color(66 / 256f, 135 / 256f, 245 / 256f, 1f);
+        levelLoader.LoadLevel1();
+    }
+}
