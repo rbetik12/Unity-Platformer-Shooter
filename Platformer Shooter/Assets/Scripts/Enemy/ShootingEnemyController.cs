52faa066d799ea4a71f33aa0f3ec9fe8bd99c84b
Vitaliy Prikota <mr.prikota@gmail.com>
2020-02-06 16:14:33 +0300

    Enemy gets damage from player.

--- Platformer Shooter/Assets/Scripts/Enemy/ShootingEnemyController.cs
+++ Platformer Shooter/Assets/Scripts/Enemy/ShootingEnemyController.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
 
 public class ShootingEnemyController : MonoBehaviour {
+
+    private float hp = 100f;
+
+    private float playerDamage = 50f;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag.Equals("Player Bullet")) {
-            Destroy(other.gameObject);
+            GetDamage();
+        }
+    }
+
+    private void GetDamage() {
+        hp -= playerDamage;
+    }
+
+    private void Update() {
+        CheckHealth();
+    }
+
+    private void CheckHealth() {
+        if (hp < 0) {
+            Destroy(this.gameObject);
         }
     }
 }
