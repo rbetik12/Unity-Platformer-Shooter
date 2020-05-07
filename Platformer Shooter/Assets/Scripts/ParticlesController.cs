cbe7e83bee1d78b5767fd226efa7e00c6684e8f8
Vitaliy Prikota <mr.prikota@gmail.com>
2020-05-07 21:04:37 +0300

    Blocks drpos particles on destroy

--- Platformer Shooter/Assets/Scripts/ParticlesController.cs
+++ Platformer Shooter/Assets/Scripts/ParticlesController.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
-
+using UnityEngine.Tilemaps;
 public class ParticlesController : MonoBehaviour {
+
+    private enum BlockType {
+        GLASS,
+        STONE,
+        BOMB
+    }
+
     [SerializeField] private ParticleSystem playerDeathParticles;
     [SerializeField] private ParticleSystem enemyDeathParticles;
     [SerializeField] private ParticleSystem playerCollisionParticles;
     [SerializeField] private ParticleSystem bulletDestroyPatricles;
+    [SerializeField] private ParticleSystem blockDestroyParticles;
 
     public void PlayerDeathParticles(Vector3 deathPosition) {
         playerDeathParticles.transform.position = deathPosition;
@@ -21,14 +29,55 @@ public class ParticlesController : MonoBehaviour {
     public void PlayerCollidePatricles(Vector3 pos, Color color) {
         pos = new Vector3(pos.x, pos.y + 0.2f, pos.z);
         ParticleSystem.MainModule ma = playerCollisionParticles.main;
-        Debug.Log(color);
         ma.startColor = new ParticleSystem.MinMaxGradient(color);
         playerCollisionParticles.transform.position = pos;
         playerCollisionParticles.Play();
     }
     public void BulletDestroyPatricles(Vector3 pos) {
-        // pos = new Vector3(pos.x, pos.y + 0.2f, pos.z);
         bulletDestroyPatricles.transform.position = pos;
         bulletDestroyPatricles.Play();
+    }
+
+    public void OnBlockDestroyParticles(Vector3 pos, Color color) {
+        ParticleSystem.MainModule ma = blockDestroyParticles.main;
+        ma.startColor = new ParticleSystem.MinMaxGradient(color);
+        blockDestroyParticles.transform.position = pos;
+        blockDestroyParticles.Play();
+    }
+
+    public void OnBlockDestroyParticles(Vector3 pos, Tile tile) {
+        ParticleSystem.MainModule ma = blockDestroyParticles.main;
+        ma.startColor = new ParticleSystem.MinMaxGradient(GetParticlesColor(tile));
+        blockDestroyParticles.transform.position = pos;
+        blockDestroyParticles.Play();
+    }
+
+    private Color GetParticlesColor(Tile tile) {
+        BlockType type = GetBlockTypeByName(tile);
+        Debug.Log(type);
+        switch (type) {
+            case BlockType.BOMB:
+                return new Color(1f, 0f, 0f);
+            case BlockType.GLASS:
+                return new Color(153 / 256f, 217 / 256f, 234 / 256f);
+            case BlockType.STONE:
+                return new Color(74 / 256f, 74 / 256f, 74 / 256f);
+            default:
+                return Color.white;
+        }
+    }
+
+    private BlockType GetBlockTypeByName(Tile tile) {
+        string blockName = tile.name;
+        switch (blockName) {
+            case "Glass":
+                return BlockType.GLASS;
+            case "stone_block":
+                return BlockType.STONE;
+            case "Bomb":
+                return BlockType.BOMB;
+            default:
+                return BlockType.BOMB;
+        }
     }
 }
--- Platformer Shooter/Assets/Scripts/Weapons/Bullet.cs
+++ Platformer Shooter/Assets/Scripts/Weapons/Bullet.cs
@@ -13,18 +13,11 @@ public class Bullet : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.name.Contains("Tilemap")) {
-            // particlesController.BulletDestroyPatricles(other.GetContact(0).point);
-            // Debug.Log($"{Convert.ToInt32(Mathf.Ceil(other.GetContact(0).point.x))} {Convert.ToInt32(Mathf.Ceil(other.GetContact(0).point.y))}");
-            // Vector3Int collisionPoint = new Vector3Int(Convert.ToInt32(Mathf.Ceil(other.GetContact(0).point.x)), Convert.ToInt32(Mathf.Ceil(other.GetContact(0).point.y)), 0);
-            // Tile tile = (Tile) tilemap.GetTile(collisionPoint);
-            // Debug.Log(tile);
-            // Debug.Log(tile.color);
-            // Tile tile1 = (Tile) tilemap.GetTile(new Vector3Int(0, 0, 0));
-            // Debug.Log(tile1);
             Vector3 hitPosition = Vector3.zero;
             foreach (ContactPoint2D hit in other.contacts) {
                 hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
                 hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                particlesController.OnBlockDestroyParticles(hitPosition, (Tile) (tilemap.GetTile(tilemap.WorldToCell(hitPosition))));
                 tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
             }
             Destroy(this.gameObject);
